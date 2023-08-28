8d6d0c584a93b1d1894455b10f60907df99f5115
fweise <public@weise-games.de>
2023-08-28 16:45:52 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@
  * MIT License https://github.com/Aplenture/FrontendJS/blob/main/LICENSE
  */
 
+export * from "./controllers";
 export * from "./core";
 export * from "./enums";
+export * from "./interfaces";
 export * from "./requests";
 export * from "./utils";
-export * from "./controllers";
 export * from "./views";

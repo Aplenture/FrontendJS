44e63e2fcbfd62141199ce4f094eba9ef89e6a40
fweise <public@weise-games.de>
2023-10-17 14:37:06 +0200

    implement file downalod

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -5,8 +5,29 @@
  * MIT License https://github.com/Aplenture/FrontendJS/blob/main/LICENSE
  */
 
+import * as CoreJS from "corejs";
 import { JSONRequest } from "../requests";
 
 export async function loadConfig<T extends any>(path = '/config.json'): Promise<T> {
     return await new JSONRequest<void, T>(path).send();
+}
+
+/*!
+ * downloads content
+ * inspired by https://stackoverflow.com/a/18197341
+ */
+export function download(file: CoreJS.File) {
+    const element = document.createElement('a');
+
+    element.id = 'download';
+    element.setAttribute('href', `data:${file.type};charset=${file.charset},` + encodeURIComponent(file.toString()));
+    element.setAttribute('download', file.name);
+
+    element.style.display = 'none';
+
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
